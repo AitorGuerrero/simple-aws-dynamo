5abf8ab19640b8071e80db2d6937c00ee332867f
Aitor Guerrero <aitor.guerrero@worldcoo.com>
2019-12-03 10:56:54 +0100

    added secondary exports

--- index.ts
+++ index.ts
@@ -1,3 +1,12 @@
+import MaxRetriesReached from "./error.max-retries-reached.class";
+import IGenerator from "./generator.interface";
 import PoweredDynamo from "./powered-dynamo.class";
+import IPoweredDynamo from "./powered-dynamo.interface";
 
 export default PoweredDynamo;
+
+export {
+	IGenerator,
+	IPoweredDynamo,
+	MaxRetriesReached,
+};
